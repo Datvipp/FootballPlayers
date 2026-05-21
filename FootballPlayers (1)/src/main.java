fd5156af4cd474282b64207afa379570a0e72283
Datvipp <huudat0922153435@gmail.com>
2026-05-22 01:15:59 +0700

    Add files via upload

--- FootballPlayers (1)/src/main.java
+++ FootballPlayers (1)/src/main.java
@@ -14,36 +14,15 @@ public class main {
     public static void main(String[] args) {
         
         Players pm=new Players();
-        TrainingSession ts=new TrainingSession();
+        TrainingSession ts = new TrainingSession();
+        Match ms = new Match();
         Scanner sc=new Scanner(System.in);
      
         pm.inputPlayers();
         pm.displayPlayer();
         ts.createTrainingSession();
         ts.displayTrainingSession();
-        MatchList ds = new MatchList();
-        int choice;
-        do {
-            System.out.println("===== MENU =====");
-            System.out.println("1. Them tran dau");
-            System.out.println("2. Xem lich su tran dau");
-            System.out.println("0. Thoat");
-            System.out.print("Nhap lua chon: ");
-            choice = sc.nextInt();
-            if(choice == 1) {
-                ds.themMatch();
-            }
-            else if(choice == 2) {
-                ds.xemMatchHistory();
-            }
-            else if(choice == 0) {
-                System.out.println("Thoat chuong trinh");
-            }
-            else {
-                System.out.println("Lua chon khong hop le");
-            }
-        } while(choice != 0);
-
-    }
+        ms.nhapMatch();
+        ms.xuatMatch();
     }
 }
