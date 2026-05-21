a3579df23fe6635f15020fd1069acdbe645f6006
Datvipp <huudat0922153435@gmail.com>
2026-05-21 21:26:17 +0700

    Add files via upload

---
+++ FootballPlayers (1)/src/main.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author manhd
+ */
+public class main {
+    public static void main(String[] args) {
+        
+        Players pm=new Players();
+        Scanner sc=new Scanner(System.in);
+     
+        pm.inputPlayers();
+        pm.displayPlayer();
+    }
+}
