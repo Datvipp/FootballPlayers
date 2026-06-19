d733ec8975af1300f6dad44a4e21758a22a92bee
manhduy1911 <manhdoduy1911@gmail.com>
2026-06-19 20:59:58 +0700

    Update ClubManager.java

--- FootballPlayers (1)/src/SERVICES/ClubManager.java
+++ FootballPlayers (1)/src/SERVICES/ClubManager.java
@@ -2,9 +2,7 @@ package SERVICES;
 import MODEL.Player;
 import MODEL.RegularPlayer;
 import MODEL.StarPlayer;
-
 import java.util.Scanner;
-
 public class ClubManager {
     Player [] arr=new Player[100];
     int count=0;
@@ -13,8 +11,32 @@ public class ClubManager {
         boolean cont= false;
         Scanner sc=new Scanner(System.in);
         do{
-            arr[count]=new Players();
-            arr[count].inputPlayers();
+            int choice;
+            do {
+                System.out.println("Player Type: 1.Regular Player  2.Star Player");
+                System.out.print("Choose Player Type: ");
+
+                while (!sc.hasNextInt()) {
+                    System.out.println("Invalid input! Please enter 1 or 2.");
+                    sc.next();
+                }
+                choice = sc.nextInt();
+
+                if (choice != 1 && choice != 2) {
+                    System.out.println("Invalid choice! Please enter 1 or 2.");
+                }
+            } while (choice != 1 && choice != 2);
+            sc.nextLine();
+
+            if (choice == 1) {
+                arr[count] = new RegularPlayer();
+                arr[count].setType("Regular Player"); // [ADD] set type tự động, khớp với object đã tạo
+            } else {
+                arr[count] = new StarPlayer();
+                arr[count].setType("Star Player"); // [ADD] set type tự động, khớp với object đã tạo
+            }
+
+            arr[count].inputPlayers(); // [ADD] giờ không hỏi type nữa vì đã set sẵn ở trên
             count++;
             System.out.println("Add more (true|false)?: ");
            
@@ -29,20 +51,16 @@ public class ClubManager {
             System.out.println("\n");
         }
     }
-
     public void updateInfo() {
         Scanner sc=new Scanner(System.in);
         
         System.out.println("Enter Player id to update");
-        int idUpdate = sc.nextInt();
-
+        String idUpdate = sc.nextLine(); // [ADD] đổi nextInt() -> nextLine() vì id giờ là String
         boolean found = false;
-
         for (int i=0; i <count; i++){
-            if(arr[i].getId() == idUpdate){
+            if(arr[i].getId().equals(idUpdate)){ // [ADD] đổi "==" -> ".equals()" vì id là String
                 System.out.println("===Enter player new information===");
                 arr[i].inputPlayers();
-
                 found = true;
                 System.out.println("Update successful!");
                 break;
@@ -50,32 +68,26 @@ public class ClubManager {
         }
         if(!found) System.out.println("Player not found");
     } 
-
-     public void deactivatePlayer(int id) {
+     public void deactivatePlayer(String id) { // [ADD] đổi tham số int -> String
         boolean found = false;
-
         for(int i = 0; i < count; i++) {
-            if(arr[i].getId() == id) {
+            if(arr[i].getId().equals(id)) { // [ADD] đổi "==" -> ".equals()"
                 arr[i].setStatus("Inactive");
                 found = true;
                 System.out.println("Player deactivated!");
                 break;
             }
         }
-
         if(found == false) {
             System.out.println("Player not found!");
         }
     }
-
-    public void searchPlayer(int id){
+    public void searchPlayer(String id){ // [ADD] đổi tham số int -> String
         boolean found = false;
-
         for (int i=0; i<count; i++){
             
-            if( arr[i].getId() == id){
+            if( arr[i].getId().equals(id)){ // [ADD] đổi "==" -> ".equals()"
                 arr[i].displayPlayer();
-
                 found = true;
                 break;
             }
@@ -84,24 +96,20 @@ public class ClubManager {
            System.out.println("Player not found!");
         }
     }
-
-    public MODEL.Players getPlayerById(int id) {
+    public MODEL.Player getPlayerById(String id) { // [ADD] đổi MODEL.Players -> MODEL.Player, int -> String
         for (int i = 0; i < count; i++) {
-            if (arr[i].getId() == id) {
+            if (arr[i].getId().equals(id)) { // [ADD] đổi "==" -> ".equals()"
                 return arr[i]; // Trả về nguyên object Player nếu tìm thấy
             }
         }
         return null; // Trả về null nếu không có cầu thủ này
     } // Q thêm vào đó
-
     // Hàm mới: Dùng để cập nhật riêng thành tích thi đấu hàng tháng (Q thêm vào đó)
     public void updatePlayerStats() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Player ID to update stats: ");
-        int searchId = sc.nextInt();
-
-        MODEL.Players p = getPlayerById(searchId);
-
+        String searchId = sc.nextLine(); // [ADD] đổi nextInt() -> nextLine()
+        MODEL.Player p = getPlayerById(searchId); // [ADD] đổi MODEL.Players -> MODEL.Player
         if (p != null) {
             System.out.println("Updating stats for player: " + p.getName());
             
@@ -116,15 +124,12 @@ public class ClubManager {
             System.out.println("Player not found!");
         }
     }
-
     // Lấy tổng số cầu thủ hiện có
     public int getCount() {
         return this.count;
     }
-
     // Lấy hồ sơ cầu thủ theo số thứ tự (để chạy vòng lặp)
-    public MODEL.Players getPlayerByIndex(int index) {
+    public MODEL.Player getPlayerByIndex(int index) { // [ADD] đổi MODEL.Players -> MODEL.Player
         return this.arr[index];
     }
 }
-
