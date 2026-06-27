a4f3014386a0b26b64c6192065880152e9a0e7c5
Nguyễn Đăng Khoa <nguyendangkhoa.150906@gmail.com>
2026-06-27 09:52:05 +0700

    Update Match.java

--- FootballPlayers (1)/src/MODEL/Match.java
+++ FootballPlayers (1)/src/MODEL/Match.java
@@ -9,6 +9,7 @@ public class Match {
     private String date;
     private String opponentTeam;
     private String matchType;
+    private String stadium;
 
     // Constructor
     public Match() {
@@ -16,14 +17,16 @@ public class Match {
         this.date = "";
         this.opponentTeam = "";
         this.matchType = "";
+        this.stadium = "";
     }
     
     // Constructor
-    public Match(int matchID, String date, String opponentTeam, String matchType) {
+    public Match(int matchID, String date, String opponentTeam, String matchType, String stadium) {
         this.matchID = matchID;
         this.date = date;
         this.opponentTeam = opponentTeam;
         this.matchType = matchType;
+        this.stadium = stadium;
     }
     
     // Getters
@@ -56,7 +59,14 @@ public class Match {
     public void setMatchType(String matchType) {
         this.matchType = matchType;
     }
-    
+
+    public String getStadium(){
+         return this.stadium; 
+    }
+
+    public void setStadium(String stadium){
+         this.stadium = stadium; 
+    }
     // Input method
     public void inputMatch() {
         inputMatch(new Scanner(System.in));
@@ -66,10 +76,12 @@ public class Match {
         System.out.print("Input Match ID: ");
         this.matchID = sc.nextInt();
         sc.nextLine();
-        System.out.print("Input date: ");
+        System.out.print("Input date (dd/MM/yyyy): ");
         this.date = sc.nextLine();
         System.out.print("Input opponent team: ");
         this.opponentTeam = sc.nextLine();
+        System.out.print("Input stadium: ");
+        this.stadium = sc.nextLine();
     }
     
     // Display method
@@ -77,5 +89,6 @@ public class Match {
         System.out.println("Match ID: " + this.matchID);
         System.out.println("Date: " + this.date);
         System.out.println("Opponent Team: " + this.opponentTeam);
+        System.out.println("Stadium: " + this.stadium);
     }
 }
