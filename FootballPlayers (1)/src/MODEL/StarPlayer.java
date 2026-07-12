01685e97ae4c0e6a66b678080e551e99f7b22578
hoangquanpham2204-bc <hoangquanpham2204@gmail.com>
2026-07-12 23:32:50 +0700

    Update StarPlayer.java

--- FootballPlayers (1)/src/MODEL/StarPlayer.java
+++ FootballPlayers (1)/src/MODEL/StarPlayer.java
@@ -4,6 +4,6 @@ package MODEL;
 public class StarPlayer extends Player {
     @Override
     public double calculateBonus(int points) {
-        return points * 500;
+        return points * 500.0;
     }
 }
