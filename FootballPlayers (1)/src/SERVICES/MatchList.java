90f1496d72569fa3f51a6057def9c2aa2d2971c2
Nguyễn Đăng Khoa <nguyendangkhoa.150906@gmail.com>
2026-07-17 21:52:23 +0700

    Update MatchList.java

--- FootballPlayers (1)/src/SERVICES/MatchList.java
+++ FootballPlayers (1)/src/SERVICES/MatchList.java
@@ -191,11 +191,11 @@ public class MatchList {
 
     //delete file
     public void deleteFile(String fileName){
-    if (MatchIO.deleteFile(fileName)) {
-        arr.clear();
-        System.out.println("File deleted successfully.");
-    } else {
-        System.out.println("File does not exist.");
+        if (MatchIO.deleteFile(fileName)) {
+            arr.clear();
+            System.out.println("File deleted successfully.");
+        } else {
+            System.out.println("File does not exist.");
+        }
     }
-}
 }
